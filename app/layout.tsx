cc16b62c04ff1e73b6debb0233e436360807add7
kanav-sharmaa <kanavv06@gmail.com>
2025-10-24 23:20:13 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type React from "react"
 import { Inter, JetBrains_Mono } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css"
-
 const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
